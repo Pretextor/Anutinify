7ca364278e9df36eb511dd9a0c061f9b4e0cb9f9
epsilon_naughty <waii18320@gmail.com>
2026-08-01 20:42:02 +0700

    Starting to implement hungarian algorithm. Test for now.

--- src/algorithm.rs
+++ src/algorithm.rs
@@ -1,8 +1,12 @@
-use pathfinder;
+use pathfinding::kuhn_munkres::kuhn_munkres;
 
-struct Processes{
 
-}
-impl Processes {
-
+fn process_hungarian() -> (i32 , Vec<usize>){
+    let weights = &[
+        [100, 110, 90],
+        [95, 130, 75],
+        [95, 140, 65],
+    ];
+    let (total_weight, assignment) = kuhn_munkres(weights);
+    (total_weight, assignment)
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ pub mod tools;
 use std::error::Error;
 use std::io;
 
-//To do: Hungarian algorithm in algorithm.rs
+//To do: genetic algorithm in algorithm.rs
 
 fn main() {
     tools::downscale_crop();
